b947ccefbbb953c63ec3c2bdbe6f71a6558a631f
d4rkonus <d4rkonus@gmail.com>
2026-03-25 17:00:48 +0000

    .

--- file.sh
+++ file.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 # Colours
 greenColour="\033[1;32m"
 endColour="\033[0m"
@@ -9,16 +7,58 @@ redColour="\033[1;31m"
 yellowColour="\033[1;33m"
 grayColour="\033[1;37m"
 
-source "$SCRIPT_DIR/functions/system.sh"
-source "$SCRIPT_DIR/functions/images.sh"
-source "$SCRIPT_DIR/functions/containers.sh"
-source "$SCRIPT_DIR/functions/ui.sh"
 
-main() {
-    sleep 1 >/dev/null 2>&1
-    check_root
-    check_docker
-    main_panel
+say_hello() {
+cat << "EOF"
+       ____            _        _                                   _             
+      / ___|___  _ __ | |_ __ _(_)_ __   ___ _ __   _ __ ___   __ _| | _____ _ __ 
+     | |   / _ \| '_ \| __/ _` | | '_ \ / _ \ '__| | '_ ` _ \ / _` | |/ / _ \ '__|
+     | |__| (_) | | | | || (_| | | | | |  __/ |    | | | | | | (_| |   <  __/ |   
+      \____\___/|_| |_|\__\__,_|_|_| |_|\___|_|    |_| |_| |_|\__,_|_|\_\___|_|   
+EOF
+                                                            echo -e "${grayColour}made by d4rkonus${endColour}\n"
 }
 
-main
+
+check_root() {
+    if [[ "$(id -u)" -ne 0 ]]; then
+        echo -e "\n${redColour}[!] Please, run as root.${endColour}"
+        exit 1
+    fi
+}
+
+check_docker() {
+    if command -v docker >/dev/null 2>&1; then
+        echo -e "${greenColour}[+] Docker is installed${endColour}"
+    else
+        echo -e "${redColour}[!] Installing Docker...${endColour}"
+
+        apt update -y >/dev/null 2>&1
+        apt install -y docker.io >/dev/null 2>&1
+
+        echo -e "${greenColour}[+] Docker installed${endColour}"
+        read -p "Press Enter to continue..."
+    fi
+}
+
+main_panel(){
+    while true; do
+        clear
+        say_hello
+
+        echo -e "\n${greenColour}1 - List images${endColour}"
+        echo -e "${greenColour}2 - List containers${endColour}"
+        echo -e "${greenColour}3 - Build image${endColour}"
+        echo -e "${greenColour}4 - Build container${endColour}"
+        echo -e "${greenColour}5 - Execute container${endColour}"
+        echo -e "${greenColour}6 - Delete containers${endColour}"
+        echo -e "${greenColour}7 - Delete images${endColour}"
+        echo -e "${greenColour}8 - Exit${endColour}"
+
+        read -p "[+] Select an option: " number
+}
+
+check_root
+say_hello
+check_docker
+main_panel
