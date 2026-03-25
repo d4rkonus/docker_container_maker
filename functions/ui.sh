94b4c584c06941375ca13c73bab47653e487aff0
d4rkonus <d4rkonus@gmail.com>
2026-03-25 14:19:50 +0000

    Update ui.sh

--- functions/ui.sh
+++ functions/ui.sh
@@ -33,6 +33,7 @@ main_panel() {
                 docker images >/dev/null 2>&1
                 echo -e ""
                 read -p "Press enter to continue..."
+                clear >/dev/null 2>&1
                 ;;
 
             2)
@@ -40,36 +41,43 @@ main_panel() {
                 docker ps -a >/dev/null 2>&1
                 echo -e ""
                 read -p "Press enter to continue..."
+                clear >/dev/null 2>&1
                 ;;
 
             3)
                 clear >/dev/null 2>&1
                 distro_select
                 read -p "Press enter to continue..."
+                clear >/dev/null 2>&1
                 ;;
 
             4)
                 clear >/dev/null 2>&1
                 container_maker
                 read -p "Press enter to continue..."
+                clear >/dev/null 2>&1
                 ;;
 
             5)
                 clear >/dev/null 2>&1
                 run_container
                 read -p "Press enter to continue..."
+                clear >/dev/null 2>&1
+
                 ;;
 
             6)
                 clear >/dev/null 2>&1
                 delete_containers
                 read -p "Press enter to continue..."
+                clear >/dev/null 2>&1
                 ;;
 
             7)
                 clear >/dev/null 2>&1
                 delete_images
                 read -p "Press enter to continue..."
+                clear >/dev/null 2>&1
                 ;;
 
             8)
